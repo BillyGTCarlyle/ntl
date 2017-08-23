e470f1d0378fb136782c469b8a6c51dbb774024c
Billy Carlyle <billy@billycarlyle.uk>
2017-08-23 11:41:45 +0000

    Reformatted main.cpp and added some comments

--- main.cpp
+++ main.cpp
@@ -9,43 +9,37 @@
 using namespace PoDoFo;
 
 void PrintHelp(){
-    std::cout << "Usage:" << std::endl;
-    std::cout << "  ntp [outputfile.pdf]" << std::endl << std::endl;
+	//If the user doesn't know what arguments are required
+	std::cout << "Usage:" << std::endl;
+	std::cout << "  ntp [outputfile.pdf] [inputfile.ntl]" << std::endl << std::endl;
 }
 int main( int argc, char* argv[] ){
-    /*
-     * Check if a filename was passed as commandline argument.
-     * If more than 1 argument or no argument is passed,
-     * a help message is displayed and the example application
-     * will quit.
-     */
-    if( argc != 4 ){
-        PrintHelp();
-        return -1;
-    }
+	/*
+	 * argv[1] should be output file name, OR help,
+	 * argv[2] should be input file name.
+	 */
+	if( argc != 3 ){
+		PrintHelp();
+		return -1;
+	}
+	if(argv[1] == "help"){
+		PrintHelp();
+		return -1;
+	}
 
-       try {
-        /*
-         * Call the drawing routing which will create a PDF file
-         * with the filename of the output file as argument.
-         */
-	parser(argv[3], argv[1]);
-    } catch( const PdfError & eCode ) {
-        /*
-         * We have to check if an error has occurred.
-         * If yes, we return and print an error message
-         * to the commandline.
-         */
-        eCode.PrintErrorMsg();
-        return eCode.GetError();
-    }
-
-    /*
-     * The PDF was created sucessfully.
-     */
-    std::cout << std::endl
-              << "Created PDF document " << argv[2] << std::endl << std::endl;
-    
-    return 0;
+	try {
+		parser(argv[2], argv[1]);
+	}catch(const PdfError & eCode){
+        
+	eCode.PrintErrorMsg();
+	return eCode.GetError();
+	}
+	/*
+	* The PDF was created sucessfully.
+	*/
+	std::cout << std::endl
+		<< "Created PDF document " << argv[1] << std::endl << std::endl;
+	
+	return 0;
 }
 
