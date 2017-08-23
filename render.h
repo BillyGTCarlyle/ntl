5a58dfac9d475e1fab46d416068ebb6c9d39ff77
Billy Carlyle <billy@billycarlyle.uk>
2017-08-23 17:07:38 +0000

    Added license notice

--- render.h
+++ render.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2017 Billy Carlyle
+ *
+ * This file is part of NTL.
+ *  
+ *   Ntl is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   NTL is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NTL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef render_h
 #pragma once
 #define render_h
